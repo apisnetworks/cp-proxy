cba3cb34f0cc070ab30d7cecf440c69378e1d637
Matt Saladna <matt@apisnetworks.com>
2020-11-28 02:36:32 -0500

    Forward HTTPS requests if present upstream Add package lock

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 "use strict";
 // Before switching servers on a request, fetch from this location
-var DEFAULT_TARGET = process.env.CP_TARGET || 'http://localhost:2082';
+var DEFAULT_TARGET = process.env.CP_TARGET || 'http://127.0.0.1:2082';
+
+var STRICT_SSL = process.env.hasOwnProperty("STRICT_SSL") ? Boolean(process.env.STRICT_SSL) : true;
 
 // Port that the proxy server will listen on
 var PORT = process.env.LISTEN_PORT || 8021;
@@ -25,9 +27,16 @@ app.use(cookieSession( {
 
 
 var proxy = httpProxy.createProxyServer({
-    target: DEFAULT_TARGET
+    target: {
+        protocol: 'https:',
+        host: 'localhost',
+        port: 2083,
+    },
+    secure: STRICT_SSL
 }).on('error', function (err, req, res) {
     res.end();
+}).on('proxyReq', function (proxyReq, req, res, options) {
+    proxyReq.setHeader('X-Forwarded-Proto', req.headers['x-forwarded-proto'] || 'https');
 });
 
 app.use(function (req, res, next) {
@@ -41,23 +50,23 @@ app.use(function (req, res, next) {
             useProxy = false;
         } else if (useProxy && hdr === 'location') {
             location = value;
-            if (res.statusCode === 307) { 
-                return oSetHeader(name, value); 
+
+            if (res.statusCode === 307) {
+                return oSetHeader(name, value);
             }
 
             if (location.charAt(6) === '/' &&
                 (location.substr(0, 7) === 'http://' || location.substr(0,8) === 'https://'))
             {
                 if (location.substr(0, 17) !== 'http://localhost:') {
-                    // ensure proto is http, otherwise upstream http server will double-encrypt
-                    // transmission
-                    req.session.target = location.substr(0, location.indexOf('/', 8)).
-                        replace("https:","http:");
+                    // server isn't local proxy
+                    req.session.target = location.substr(0, location.indexOf('/', 8));
                 }
 
                 value = location.substr(location.indexOf('/', 8));
             }
         }
+
         return oSetHeader(name, value);
     };
     next();
@@ -65,7 +74,8 @@ app.use(function (req, res, next) {
 
 app.use(function (req, res) {
     proxy.web(req, res, {
-        target: req.session.target != 'http://localhost:' + PORT &&
+        target: req.session.target != 'https://localhost:' + PORT &&
+            req.session.target !== 'http://localhost:' + PORT &&
             req.session.target || DEFAULT_TARGET
     });
 });
